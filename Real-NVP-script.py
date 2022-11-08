b53e7ddad573400009d348008b99a2c0d922188c
His-Royal-Dudeness <zuckerdave@gmail.com>
2022-11-08 07:41:51 +0200

    Update Real-NVP-script.py

--- Real-NVP-script.py
+++ Real-NVP-script.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_moons
 data = make_moons(10000, noise=0.05)[0]
 
 epochs = 1500
-num_of_hidden_layers = 2  #number of layers in each cell
+num_of_hidden_layers = 3  #number of layers in each cell
 hidden_layers_size = 30 #number of nodes in each layer
 num_of_cells = 7 #number of cells in the flow - number of affine transformations
 learning_rate = 1e-2
